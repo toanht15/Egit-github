ad6e2eb231b381900bd3e2f1538d2025a4e69441
ToanHT <thetoanpr04@gmail.com>
2014-07-15 23:48:29 +0700

    commit

--- de.vogella.git.first/src/de/vogella/git/first/GitTest.java
+++ de.vogella.git.first/src/de/vogella/git/first/GitTest.java
@@ -2,6 +2,6 @@ package de.vogella.git.first;
 
 public class GitTest {
 	public static void main(String[] args){
-		System.out.println("I am SunKid");
+		System.out.println("Hoang The Toan");
 	}
 }
